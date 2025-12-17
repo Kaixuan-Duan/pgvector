99d11abee2562d2844f5abd5a9aa386687e571b6
dkx <1417048384@qq..com>
2025-12-17 03:22:39 +0000

    1.Plugin name change: vector -> hybrid_vector 2.Usage name change: USING hnsw -> USING hydex

--- sql/hybrid_vector--0.4.4--0.5.0.sql
+++ sql/hybrid_vector--0.4.4--0.5.0.sql
@@ -22,22 +22,22 @@ CREATE AGGREGATE sum(vector) (
 CREATE FUNCTION hnswhandler(internal) RETURNS index_am_handler
 	AS 'MODULE_PATHNAME' LANGUAGE C;
 
-CREATE ACCESS METHOD hnsw TYPE INDEX HANDLER hnswhandler;
+CREATE ACCESS METHOD hydex TYPE INDEX HANDLER hnswhandler;
 
-COMMENT ON ACCESS METHOD hnsw IS 'hnsw index access method';
+COMMENT ON ACCESS METHOD hydex IS 'hydex index access method';
 
 CREATE OPERATOR CLASS vector_l2_ops
-	FOR TYPE vector USING hnsw AS
+	FOR TYPE vector USING hydex AS
 	OPERATOR 1 <-> (vector, vector) FOR ORDER BY float_ops,
 	FUNCTION 1 vector_l2_squared_distance(vector, vector);
 
 CREATE OPERATOR CLASS vector_ip_ops
-	FOR TYPE vector USING hnsw AS
+	FOR TYPE vector USING hydex AS
 	OPERATOR 1 <#> (vector, vector) FOR ORDER BY float_ops,
 	FUNCTION 1 vector_negative_inner_product(vector, vector);
 
 CREATE OPERATOR CLASS vector_cosine_ops
-	FOR TYPE vector USING hnsw AS
+	FOR TYPE vector USING hydex AS
 	OPERATOR 1 <=> (vector, vector) FOR ORDER BY float_ops,
 	FUNCTION 1 vector_negative_inner_product(vector, vector),
 	FUNCTION 2 vector_norm(vector);
--- sql/hybrid_vector--0.8.0.sql
+++ sql/hybrid_vector--0.8.0.sql
@@ -257,9 +257,9 @@ COMMENT ON ACCESS METHOD ivfflat IS 'ivfflat index access method';
 CREATE FUNCTION hnswhandler(internal) RETURNS index_am_handler
 	AS 'MODULE_PATHNAME' LANGUAGE C;
 
-CREATE ACCESS METHOD hnsw TYPE INDEX HANDLER hnswhandler;
+CREATE ACCESS METHOD hydex TYPE INDEX HANDLER hnswhandler;
 
-COMMENT ON ACCESS METHOD hnsw IS 'hnsw index access method';
+COMMENT ON ACCESS METHOD hydex IS 'hydex index access method';
 
 -- access method private functions
 
@@ -311,23 +311,23 @@ CREATE OPERATOR CLASS vector_cosine_ops
 	FUNCTION 4 vector_norm(vector);
 
 CREATE OPERATOR CLASS vector_l2_ops
-	FOR TYPE vector USING hnsw AS
+	FOR TYPE vector USING hydex AS
 	OPERATOR 1 <-> (vector, vector) FOR ORDER BY float_ops,
 	FUNCTION 1 vector_l2_squared_distance(vector, vector);
 
 CREATE OPERATOR CLASS vector_ip_ops
-	FOR TYPE vector USING hnsw AS
+	FOR TYPE vector USING hydex AS
 	OPERATOR 1 <#> (vector, vector) FOR ORDER BY float_ops,
 	FUNCTION 1 vector_negative_inner_product(vector, vector);
 
 CREATE OPERATOR CLASS vector_cosine_ops
-	FOR TYPE vector USING hnsw AS
+	FOR TYPE vector USING hydex AS
 	OPERATOR 1 <=> (vector, vector) FOR ORDER BY float_ops,
 	FUNCTION 1 vector_negative_inner_product(vector, vector),
 	FUNCTION 2 vector_norm(vector);
 
 CREATE OPERATOR CLASS vector_l1_ops
-	FOR TYPE vector USING hnsw AS
+	FOR TYPE vector USING hydex AS
 	OPERATOR 1 <+> (vector, vector) FOR ORDER BY float_ops,
 	FUNCTION 1 l1_distance(vector, vector);
 
@@ -623,26 +623,26 @@ CREATE OPERATOR CLASS halfvec_cosine_ops
 	FUNCTION 5 ivfflat_halfvec_support(internal);
 
 CREATE OPERATOR CLASS halfvec_l2_ops
-	FOR TYPE halfvec USING hnsw AS
+	FOR TYPE halfvec USING hydex AS
 	OPERATOR 1 <-> (halfvec, halfvec) FOR ORDER BY float_ops,
 	FUNCTION 1 halfvec_l2_squared_distance(halfvec, halfvec),
 	FUNCTION 3 hnsw_halfvec_support(internal);
 
 CREATE OPERATOR CLASS halfvec_ip_ops
-	FOR TYPE halfvec USING hnsw AS
+	FOR TYPE halfvec USING hydex AS
 	OPERATOR 1 <#> (halfvec, halfvec) FOR ORDER BY float_ops,
 	FUNCTION 1 halfvec_negative_inner_product(halfvec, halfvec),
 	FUNCTION 3 hnsw_halfvec_support(internal);
 
 CREATE OPERATOR CLASS halfvec_cosine_ops
-	FOR TYPE halfvec USING hnsw AS
+	FOR TYPE halfvec USING hydex AS
 	OPERATOR 1 <=> (halfvec, halfvec) FOR ORDER BY float_ops,
 	FUNCTION 1 halfvec_negative_inner_product(halfvec, halfvec),
 	FUNCTION 2 l2_norm(halfvec),
 	FUNCTION 3 hnsw_halfvec_support(internal);
 
 CREATE OPERATOR CLASS halfvec_l1_ops
-	FOR TYPE halfvec USING hnsw AS
+	FOR TYPE halfvec USING hydex AS
 	OPERATOR 1 <+> (halfvec, halfvec) FOR ORDER BY float_ops,
 	FUNCTION 1 l1_distance(halfvec, halfvec),
 	FUNCTION 3 hnsw_halfvec_support(internal);
@@ -677,13 +677,13 @@ CREATE OPERATOR CLASS bit_hamming_ops
 	FUNCTION 5 ivfflat_bit_support(internal);
 
 CREATE OPERATOR CLASS bit_hamming_ops
-	FOR TYPE bit USING hnsw AS
+	FOR TYPE bit USING hydex AS
 	OPERATOR 1 <~> (bit, bit) FOR ORDER BY float_ops,
 	FUNCTION 1 hamming_distance(bit, bit),
 	FUNCTION 3 hnsw_bit_support(internal);
 
 CREATE OPERATOR CLASS bit_jaccard_ops
-	FOR TYPE bit USING hnsw AS
+	FOR TYPE bit USING hydex AS
 	OPERATOR 1 <%> (bit, bit) FOR ORDER BY float_ops,
 	FUNCTION 1 jaccard_distance(bit, bit),
 	FUNCTION 3 hnsw_bit_support(internal);
@@ -893,26 +893,26 @@ CREATE OPERATOR CLASS sparsevec_ops
 	FUNCTION 1 sparsevec_cmp(sparsevec, sparsevec);
 
 CREATE OPERATOR CLASS sparsevec_l2_ops
-	FOR TYPE sparsevec USING hnsw AS
+	FOR TYPE sparsevec USING hydex AS
 	OPERATOR 1 <-> (sparsevec, sparsevec) FOR ORDER BY float_ops,
 	FUNCTION 1 sparsevec_l2_squared_distance(sparsevec, sparsevec),
 	FUNCTION 3 hnsw_sparsevec_support(internal);
 
 CREATE OPERATOR CLASS sparsevec_ip_ops
-	FOR TYPE sparsevec USING hnsw AS
+	FOR TYPE sparsevec USING hydex AS
 	OPERATOR 1 <#> (sparsevec, sparsevec) FOR ORDER BY float_ops,
 	FUNCTION 1 sparsevec_negative_inner_product(sparsevec, sparsevec),
 	FUNCTION 3 hnsw_sparsevec_support(internal);
 
 CREATE OPERATOR CLASS sparsevec_cosine_ops
-	FOR TYPE sparsevec USING hnsw AS
+	FOR TYPE sparsevec USING hydex AS
 	OPERATOR 1 <=> (sparsevec, sparsevec) FOR ORDER BY float_ops,
 	FUNCTION 1 sparsevec_negative_inner_product(sparsevec, sparsevec),
 	FUNCTION 2 l2_norm(sparsevec),
 	FUNCTION 3 hnsw_sparsevec_support(internal);
 
 CREATE OPERATOR CLASS sparsevec_l1_ops
-	FOR TYPE sparsevec USING hnsw AS
+	FOR TYPE sparsevec USING hydex AS
 	OPERATOR 1 <+> (sparsevec, sparsevec) FOR ORDER BY float_ops,
 	FUNCTION 1 l1_distance(sparsevec, sparsevec),
 	FUNCTION 3 hnsw_sparsevec_support(internal);
